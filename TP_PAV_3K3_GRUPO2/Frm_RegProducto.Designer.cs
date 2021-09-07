c13df8a9964cf2d7fde1d0dd8aac117b526c609c
Luz González <luzgonzalezutn@gmail.com>
2021-09-07 15:46:21 -0300

    Se realiza la creación del repositorio.

---
+++ TP_PAV_3K3_GRUPO2/Frm_RegProducto.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace TP_PAV_3K3_GRUPO2
+{
+    partial class Frm_RegProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Des = new System.Windows.Forms.TextBox();
+            this.txt_venta = new System.Windows.Forms.TextBox();
+            this.txt_costo = new System.Windows.Forms.TextBox();
+            this.txt_stock = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_rubro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_marca = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripción:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Stock:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio venta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio costo:";
+            // 
+            // txt_Des
+            // 
+            this.txt_Des.Location = new System.Drawing.Point(187, 42);
+            this.txt_Des.Name = "txt_Des";
+            this.txt_Des.Size = new System.Drawing.Size(153, 26);
+            this.txt_Des.TabIndex = 4;
+            // 
+            // txt_venta
+            // 
+            this.txt_venta.Location = new System.Drawing.Point(187, 192);
+            this.txt_venta.Name = "txt_venta";
+            this.txt_venta.Size = new System.Drawing.Size(153, 26);
+            this.txt_venta.TabIndex = 5;
+            // 
+            // txt_costo
+            // 
+            this.txt_costo.Location = new System.Drawing.Point(187, 154);
+            this.txt_costo.Name = "txt_costo";
+            this.txt_costo.Size = new System.Drawing.Size(153, 26);
+            this.txt_costo.TabIndex = 6;
+            // 
+            // txt_stock
+            // 
+            this.txt_stock.Location = new System.Drawing.Point(187, 224);
+            this.txt_stock.Name = "txt_stock";
+            this.txt_stock.Size = new System.Drawing.Size(153, 26);
+            this.txt_stock.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txt_rubro
+            // 
+            this.txt_rubro.Location = new System.Drawing.Point(187, 77);
+            this.txt_rubro.Name = "txt_rubro";
+            this.txt_rubro.Size = new System.Drawing.Size(153, 26);
+            this.txt_rubro.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(117, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Rubro:";
+            // 
+            // txt_marca
+            // 
+            this.txt_marca.Location = new System.Drawing.Point(187, 115);
+            this.txt_marca.Name = "txt_marca";
+            this.txt_marca.Size = new System.Drawing.Size(153, 26);
+            this.txt_marca.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Marca:";
+            // 
+            // Frm_RegProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 290);
+            this.Controls.Add(this.txt_marca);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_rubro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_stock);
+            this.Controls.Add(this.txt_costo);
+            this.Controls.Add(this.txt_venta);
+            this.Controls.Add(this.txt_Des);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_RegProducto";
+            this.Text = "Registrar Producto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Des;
+        private System.Windows.Forms.TextBox txt_venta;
+        private System.Windows.Forms.TextBox txt_costo;
+        private System.Windows.Forms.TextBox txt_stock;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_rubro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_marca;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ TP_PAV_3K3_GRUPO2/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace TP_PAV_3K3_GRUPO2
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btm_ingresar = new System.Windows.Forms.Button();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_clave = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "usuario:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "clave:";
+            // 
+            // Btm_ingresar
+            // 
+            this.Btm_ingresar.Location = new System.Drawing.Point(411, 116);
+            this.Btm_ingresar.Name = "Btm_ingresar";
+            this.Btm_ingresar.Size = new System.Drawing.Size(97, 43);
+            this.Btm_ingresar.TabIndex = 3;
+            this.Btm_ingresar.Text = "Ingresar";
+            this.Btm_ingresar.UseVisualStyleBackColor = true;
+            this.Btm_ingresar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(262, 52);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(119, 26);
+            this.txt_usuario.TabIndex = 4;
+            // 
+            // txt_clave
+            // 
+            this.txt_clave.Location = new System.Drawing.Point(262, 92);
+            this.txt_clave.Name = "txt_clave";
+            this.txt_clave.Size = new System.Drawing.Size(119, 26);
+            this.txt_clave.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 122);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 171);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_clave);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.Btm_ingresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btm_ingresar;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_clave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}

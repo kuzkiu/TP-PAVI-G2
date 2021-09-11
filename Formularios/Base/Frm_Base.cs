c37f3c04ebe81f3b92ac2f6b56e693310f820f5b
PocheG <90357206+PocheG@users.noreply.github.com>
2021-09-11 20:20:03 -0300

    Add files via upload
    Agregué carpetas para dividir lo que vayamos programando como dijo en la segunda clase. Dentro de la carpeta formularios puse un formulario base para hacer los abmc (le falta toda la programación igual)

---
+++ Formularios/Base/Frm_Base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_PAV_3K3_GRUPO2.Formularios.Base
+{
+    public partial class Frm_Base : Form
+    {
+        public Frm_Base()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Formularios/Clientes/Frm_ClienteBase.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace TP_PAV_3K3_GRUPO2.Formularios.Clientes
+{
+    partial class Frm_ClienteBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_ClienteBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 502);
+            this.Name = "Frm_ClienteBase";
+            this.Text = "Frm_ClienteBase";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
